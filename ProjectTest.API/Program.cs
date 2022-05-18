8795fe8940eea5a0d4586a68f74185f7ca131829
Stywar <franco111988@gmail.com>
2022-05-18 08:54:37 -0500

    add new files

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -1,26 +1,23 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace ProjectTest.API
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.MapGet("/", () =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    app.Logger.LogInformation("Initial Home Get");
+    return "GET - Net 6 Running ... 09052022-v1.0.0 CI/CD in JENKINS JOB v1.0.0 AFORO255";
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+app.MapPost("/", () =>
+{
+    app.Logger.LogInformation("Initial Home Post");
+    return "POST - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
+});
+
+app.Run();
+
